cb2548c214ce5c752b8f79a06dc3f1fbf48c1765
Erin Pintozzi <erin.pintozzi@gmail.com>
2016-10-24 20:39:22 -0600

    Create login/authentication functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,23 @@
 class SessionsController < ApplicationController
+  include SessionsHelper
 
-def login
-  @user = User.all
-  User.authenticate(params[:email], params[:password])
+def new
+  # @user = User.all
 end
 
-def logout
+def create
+  user = User.find_by(email: params[:session][:email].downcase)
+  # if user && user.authenticate(params[:session][:email], params[:session][:password])
+  if User.authenticate(params[:session][:email], params[:session][:password])
+    log_in user
+    redirect_to user_path(user)
+  else
+    flash.now[:danger] = "Invalid email or password"
+    render :new
+  end
+end
+
+def destroy
 
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
-  resources :users, only: [:new, :create, :index]
+  resources :users, only: [:new, :create, :index, :show]
+  resources :session, only: [:new, :create, :delete]
 
-  get '/login' => 'sessions#login'
-  get '/logout' => 'sessions#logout'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 
 end
